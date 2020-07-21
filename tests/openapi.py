5fee29716d3f6fcdba3bd2ee7c24c335c4842c92
Oliver Berger <oliver@digitalarchitekt.de>
2020-07-21 15:30:54 +0200

    moved from buvar

---
+++ tests/openapi.py
@@ -0,0 +1,24 @@
+import aiohttp.web
+
+from buvar import plugin, di
+from buvar_aiohttp import openapi, json
+
+operation_map = openapi.OperationMap()
+
+
+@operation_map.register()
+async def post_foo(request):
+    raise aiohttp.web.HTTPFound(
+        location=request.app.router["get_bar"].url_for(id="1234")
+    )
+
+
+@operation_map.register()
+async def get_bar(request):
+    op = await di.nject(openapi.Operation, request=request)
+    return json.response({"foo": "bar", "operation": op})
+
+
+async def prepare(load: plugin.Loader):
+    await load("buvar_aiohttp.openapi", "buvar_aiohttp.json")
+    await operation_map.mount(ui=True)
